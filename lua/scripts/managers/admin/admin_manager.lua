45d96786af00b734f2c29f8e8e67e8164bc95935
angaityel <161797572+angaityel@users.noreply.github.com>
2024-03-17 08:32:48 +0300

    Add original lua files

---
+++ lua/scripts/managers/admin/admin_manager.lua
@@ -0,0 +1,333 @@
+﻿-- chunkname: @scripts/managers/admin/admin_manager.lua
+
+require("scripts/managers/admin/auto_rules")
+require("scripts/managers/admin/auto_balancer")
+require("scripts/managers/admin/server_stats_printer")
+require("scripts/managers/admin/network_performance_rules")
+require("scripts/managers/admin/script_rcon_server")
+require("scripts/managers/admin/speed_hack_detector")
+require("scripts/managers/game_mode/spawn_modes/pulse_spawning")
+require("scripts/managers/game_mode/spawn_modes/personal_spawning")
+require("scripts/managers/admin/script_anti_cheat")
+
+local KICK_TIMEOUT = 3
+local RCON_ADMIN_TIME = 300
+
+AdminManager = class(AdminManager)
+
+function AdminManager:init(settings)
+	self._settings = settings
+	self._server_locked = false
+
+	self:_setup_spawning(settings.spawning)
+	self:_setup_friendly_fire(settings)
+	self:_setup_max_squad_size(settings)
+	self:_setup_reserved_slots(settings)
+
+	self._auto_rules = AutoRules:new(settings)
+	self._auto_balancer = AutoBalancer:new(settings.auto_balance)
+	self._server_stats_printer = ServerStatsPrinter:new()
+	self._network_performance_rules = NetworkPerformanceRules:new(settings)
+	self._rcon_server = ScriptRconServer:new(settings.rcon)
+	settings.anti_cheat = settings.anti_cheat or {}
+	self._speed_hack_detector = SpeedHackDetector:new(settings.anti_cheat.speedhack)
+	self._anti_cheat = ScriptAntiCheat:new(settings.anti_cheat)
+	self._kick_list = {}
+end
+
+function AdminManager:register_peer(network_id, anti_cheat_key)
+	self._anti_cheat:register_peer(network_id, anti_cheat_key)
+end
+
+function AdminManager:unregister_peer(network_id)
+	self._anti_cheat:unregister_peer(network_id)
+end
+
+function AdminManager:settings()
+	return self._settings
+end
+
+function AdminManager:setup()
+	self._auto_rules:setup()
+	self._auto_balancer:setup()
+	self._speed_hack_detector:setup()
+end
+
+function AdminManager:_setup_spawning(settings)
+	self._spawning_settings = settings
+end
+
+function AdminManager:_setup_friendly_fire(settings)
+	if settings.friendly_fire then
+		local melee = settings.friendly_fire.melee or {}
+		local ranged = settings.friendly_fire.ranged or {}
+
+		self:set_friendly_fire(melee.value, melee.mirror, ranged.value, ranged.mirror)
+	end
+end
+
+function AdminManager:_setup_max_squad_size(settings)
+	local max_squad_size = settings.server_init_settings.max_squad_size or 8
+
+	TeamSettings.red.max_squad_members = math.clamp(max_squad_size, 0, math.huge)
+	TeamSettings.white.max_squad_members = math.clamp(max_squad_size, 0, math.huge)
+end
+
+function AdminManager:_setup_reserved_slots(settings)
+	local num_reserved_slots = table.size(Application.user_setting("reserved_slots") or {})
+
+	settings.server_init_settings.max_players = settings.server_init_settings.max_players + num_reserved_slots
+end
+
+function AdminManager:spawning()
+	return self._spawning_settings
+end
+
+function AdminManager:update(dt, t)
+	Profiler.start("AdminManager")
+	self._auto_rules:update(dt, t)
+	self._auto_balancer:update(dt, t)
+	self._server_stats_printer:update(dt, t)
+	self._network_performance_rules:update(dt, t)
+	self._rcon_server:update(dt, t)
+	self:_update_kick_list(dt, t)
+	self:_update_rcon_admin(dt, t)
+	self._anti_cheat:update(dt, Managers.time:time("main"))
+	Profiler.stop()
+end
+
+function AdminManager:kick_player(peer_id, reason)
+	local kick_list = self._kick_list
+
+	if kick_list[peer_id] then
+		return
+	end
+
+	kick_list[peer_id] = KICK_TIMEOUT + Managers.time:time("game")
+
+	RPC.rpc_kicked(peer_id, reason)
+end
+
+function AdminManager:_update_kick_list(dt, t)
+	local kick_list = self._kick_list
+
+	for peer_id, kick_time in pairs(kick_list) do
+		if kick_time <= t then
+			Managers.lobby:kick_member(peer_id)
+
+			kick_list[peer_id] = nil
+		end
+	end
+end
+
+function AdminManager:_update_rcon_admin(dt, t)
+	if self._rcon_admin and t >= self._rcon_admin_time and Managers.state.network:game() then
+		local player = Managers.state.network:player_from_peer_id(self._rcon_admin)
+
+		if player then
+			RPC.rpc_rcon_logged_out(self._rcon_admin, player:player_id())
+			RPC.rpc_rcon_reply(self._rcon_admin, "You have been logged out from RCON")
+		end
+
+		self:set_rcon_admin(nil)
+	end
+end
+
+function AdminManager:is_player_banned(peer_id)
+	local ban = Application.user_setting("banned_players", peer_id)
+	local time = os.time(os.date("*t"))
+
+	if ban then
+		if type(ban) == "string" then
+			local ban_string = string.split(ban, ":")
+			local ban_time = tonumber(ban_string[1])
+			local reason = ban_string[2]
+
+			if ban_time and (time < ban_time or ban_time == -1) then
+				return true
+			else
+				Application.set_user_setting("banned_players", peer_id, nil)
+				Application.save_user_settings()
+
+				return false
+			end
+		else
+			return true
+		end
+	else
+		return false
+	end
+end
+
+function AdminManager:ban_player(peer_id, reason, duration)
+	local name
+
+	for _, player in pairs(Managers.player:players()) do
+		if player:network_id() == peer_id then
+			name = player:name()
+
+			break
+		end
+	end
+
+	local time = -1
+
+	if duration then
+		time = os.time(os.date("*t")) + duration
+	end
+
+	Application.set_user_setting("banned_players", peer_id, time .. ":" .. reason)
+
+	if name then
+		Application.set_user_setting("banned_player_names", name, peer_id)
+	end
+
+	Application.save_user_settings()
+	self:kick_player(peer_id, reason or "BANNED")
+end
+
+function AdminManager:unban_player(peer_id_or_name)
+	local banned_by_id = Application.user_setting("banned_players", peer_id_or_name)
+	local banned_by_name = Application.user_setting("banned_player_names", peer_id_or_name)
+	local ban
+
+	if not banned_by_id then
+		ban = Application.user_setting("banned_players", banned_by_name)
+	else
+		ban = banned_by_id
+	end
+
+	if ban then
+		if not banned_by_id and banned_by_name then
+			Application.set_user_setting("banned_player_names", peer_id_or_name, nil)
+			Application.set_user_setting("banned_players", banned_by_name, nil)
+		else
+			Application.set_user_setting("banned_players", peer_id_or_name, nil)
+		end
+
+		Application.save_user_settings()
+	else
+		return peer_id_or_name
+	end
+end
+
+function AdminManager:set_friendly_fire(melee_value, melee_mirror, ranged_value, ranged_mirror)
+	if melee_value ~= nil then
+		AttackDamageRangeTypes.melee.friendly_fire_multiplier = math.clamp(tonumber(melee_value), 0, 1)
+	end
+
+	if melee_mirror ~= nil then
+		AttackDamageRangeTypes.melee.mirrored = to_boolean(melee_mirror)
+	end
+
+	if ranged_value ~= nil then
+		AttackDamageRangeTypes.small_projectile.friendly_fire_multiplier = math.clamp(tonumber(ranged_value), 0, 1)
+	end
+
+	if ranged_mirror ~= nil then
+		AttackDamageRangeTypes.small_projectile.mirrored = to_boolean(ranged_mirror)
+	end
+
+	return AttackDamageRangeTypes.melee.friendly_fire_multiplier, AttackDamageRangeTypes.melee.mirrored, AttackDamageRangeTypes.small_projectile.friendly_fire_multiplier, AttackDamageRangeTypes.small_projectile.mirrored
+end
+
+function AdminManager:set_max_squad_size(max_squad_size)
+	TeamSettings.red.max_squad_size = math.clamp(max_squad_size, 0, math.huge)
+	TeamSettings.white.max_squad_size = math.clamp(max_squad_size, 0, math.huge)
+
+	Managers.state.team:team_by_name("red"):set_max_squad_size(max_squad_size)
+	Managers.state.team:team_by_name("white"):set_max_squad_size(max_squad_size)
+	Managers.state.network:send_rpc_clients("rpc_set_max_squad_size", max_squad_size)
+end
+
+function AdminManager:lock_server()
+	self._server_locked = true
+end
+
+function AdminManager:unlock_server()
+	self._server_locked = false
+end
+
+function AdminManager:is_server_locked()
+	return self._server_locked
+end
+
+function AdminManager:is_server_password_protected()
+	local password = self._settings.server_init_settings.password
+
+	return password and password ~= ""
+end
+
+function AdminManager:map_rotation_settings(level_name, game_mode)
+	for _, settings in pairs(self._settings.map_rotation.maps) do
+		if settings.level == level_name and settings.game_mode == game_mode then
+			return settings
+		end
+	end
+end
+
+function AdminManager:is_demo_player_allowed(game_mode_key)
+	return DemoSettings.available_game_modes[game_mode_key] and not self._settings.kick_demo_users
+end
+
+function AdminManager:kill_player(peer_id)
+	local player = Managers.player:player_from_network_id(peer_id)
+
+	if Unit.alive(player.player_unit) then
+		ScriptUnit.extension(player.player_unit, "damage_system"):die()
+	end
+end
+
+function AdminManager:add_reserved_slot(peer_id)
+	local reserved_slots = Application.user_setting("reserved_slots") or {}
+
+	if table.contains(reserved_slots, peer_id) then
+		return false
+	end
+
+	reserved_slots[#reserved_slots + 1] = peer_id
+
+	Application.set_user_setting("reserved_slots", reserved_slots)
+	Application.save_user_settings()
+	self:_setup_reserved_slots(self._settings)
+
+	return true
+end
+
+function AdminManager:remove_reserved_slot(peer_id)
+	local reserved_slots = Application.user_setting("reserved_slots") or {}
+	local key = table.find(reserved_slots, peer_id)
+
+	if not key then
+		return false
+	end
+
+	table.remove(reserved_slots, key)
+	Application.set_user_setting("reserved_slots", reserved_slots)
+	Application.save_user_settings()
+	self:_setup_reserved_slots(self._settings)
+
+	return true
+end
+
+function AdminManager:check_reserved_slots(peer_id)
+	local players = table.size(Managers.player:players())
+	local reserved_slots = Application.user_setting("reserved_slots") or {}
+	local num_reserved_slots = table.size(reserved_slots)
+	local max_players_without_reserved_slots = self._settings.server_init_settings.max_players - num_reserved_slots
+
+	if players < max_players_without_reserved_slots then
+		return true
+	end
+
+	return table.contains(reserved_slots, peer_id)
+end
+
+function AdminManager:set_rcon_admin(peer_id)
+	self._rcon_admin = peer_id
+	self._rcon_admin_time = Managers.time:time("game") + RCON_ADMIN_TIME
+end
+
+function AdminManager:rcon_admin()
+	return self._rcon_admin
+end
---
+++ lua/scripts/managers/persistence/persistence_manager_server.lua
@@ -0,0 +1,380 @@
+﻿-- chunkname: @scripts/managers/persistence/persistence_manager_server.lua
+
+require("scripts/managers/persistence/persistence_manager_common")
+require("scripts/managers/persistence/telemetry_collector")
+require("scripts/managers/persistence/telemetry_collector_offline")
+require("scripts/managers/persistence/external_tweaks")
+require("scripts/managers/persistence/external_tweaks_offline")
+require("scripts/settings/achievements")
+require("scripts/settings/prizes")
+require("scripts/settings/medals")
+require("scripts/settings/server_browser_score")
+
+PersistenceManagerServer = class(PersistenceManagerServer, PersistenceManagerCommon)
+
+function PersistenceManagerServer:init(settings)
+	PersistenceManagerServer.super.init(self)
+	fassert(settings, "No backend settings supplied")
+
+	self._state = "uninitialized"
+
+	Managers.admin:lock_server()
+
+	self._settings = {
+		connection_type = Backend.DEDICATED_TRUSTED,
+		callback = callback(self, "cb_connected")
+	}
+
+	table.merge(self._settings, settings)
+
+	self._settings.save_progress = T(settings.save_progress, true)
+end
+
+function PersistenceManagerServer:post_init()
+	local app_id = SteamGameServer.app_id()
+	local backend_address
+
+	if script_data.settings.content_revision then
+		if app_id == 42160 then
+			backend_address = GameSettingsDevelopment.backend_address or "ftdata.fatshark.se"
+		else
+			backend_address = GameSettingsDevelopment.backend_address or "fttest01.fatshark.se"
+		end
+	elseif Application.build() == "dev" or Application.build() == "debug" then
+		backend_address = GameSettingsDevelopment.backend_address or "fttest01.fatshark.se"
+	end
+
+	self._telemetry = self._settings.telemetry.enabled == true and TelemetryCollector:new(self._settings.telemetry.template_name) or TelemetryCollectorOffline:new()
+	self._external_tweaks = self._settings.external_tweaks == true and ExternalTweaks:new(self._settings.connection.address) or ExternalTweaksOffline:new()
+	self._settings.connection.address = backend_address
+	self._settings.project_id = app_id
+
+	self:connect()
+end
+
+function PersistenceManagerServer:connect()
+	cprint("[Backend]", "Connecting to", self._settings.connection.address)
+
+	self._state = "connecting"
+
+	Managers.backend:connect(self._settings.connection.address, self._settings.project_id, self._settings.connection_type, self._settings.connection.port, self._settings.connection.interface, self._settings.callback)
+end
+
+function PersistenceManagerServer:cb_connected(response)
+	if response.error == nil then
+		cprint("[Backend]", "Authenticating...")
+
+		self._state = "authenticating"
+
+		Managers.backend:steam_login(callback(self, "cb_authenticated"))
+	else
+		cprint("[Backend]", "Error (cb_connected)", response.error)
+
+		self._state = "error"
+	end
+end
+
+function PersistenceManagerServer:cb_authenticated(response)
+	if response.error == nil then
+		local _, profile = next(response.profiles)
+
+		cprint("[Backend]", "Selecting server profile...")
+
+		self._state = "selecting_profile"
+
+		Managers.backend:select_profile(profile.id, callback(self, "cb_profile_selected"))
+	else
+		cprint("[Backend]", "Error (cb_authenticated)", response.error)
+
+		self._state = "error"
+	end
+end
+
+function PersistenceManagerServer:cb_profile_created(response)
+	if response.error == nil then
+		cprint("[Backend]", "Fetching entity types...")
+
+		self._state = "fetching_entities"
+
+		Managers.backend:select_profile(response.profile.id, callback(self, "cb_entity_types_fetched"))
+	else
+		cprint("[Backend]", "Error (cb_profile_created)", response.error)
+
+		self._state = "error"
+	end
+end
+
+function PersistenceManagerServer:cb_profile_selected(response)
+	if response.error == nil then
+		cprint("[Backend]", "Fetching entity types...")
+
+		self._state = "fetching_entities"
+
+		Managers.backend:get_entity_types(callback(self, "cb_entity_types_fetched"))
+	else
+		cprint("[Backend]", "Error (cb_fetch_entity_types)", response.error)
+
+		self._state = "error"
+	end
+end
+
+function PersistenceManagerServer:cb_entity_types_fetched(response)
+	if response.error == nil then
+		cprint("[Backend]", "Parsing entity types...")
+		self:_parse_entity_types(response.entity_types or {})
+		cprint("[Backend]", "Backend setup successfully!")
+
+		self._state = "connected"
+
+		Managers.admin:unlock_server()
+
+		if Managers.state.event then
+			Managers.chat:send_chat_message(1, "Successfully connected to the backend.")
+		end
+	else
+		cprint("[Backend]", "Error (cb_entity_types_fetched)", response.error)
+
+		self._state = "error"
+	end
+end
+
+function PersistenceManagerServer:set_save_progress(flag)
+	self._settings.save_progress = flag
+end
+
+function PersistenceManagerServer:setup(stats_collection)
+	self._stats = stats_collection
+	self._profiles = {}
+	self._achievements = Achievements:new(stats_collection)
+	self._prizes = Prizes:new(stats_collection)
+	self._medals = Medals:new(stats_collection)
+	self._ranks = Ranks:new(stats_collection)
+	self._server_browser_score = ServerBrowserScore:new(stats_collection)
+
+	if Managers.backend:connected() then
+		self._telemetry:setup()
+		self._external_tweaks:refresh()
+		Managers.state.event:register(self, "player_joined", "event_player_joined")
+	else
+		self:connect()
+	end
+end
+
+function PersistenceManagerServer:event_player_joined(player)
+	local profile_id = player.backend_profile_id
+
+	if player.remote and profile_id ~= -1 then
+		player.round_time_joined = Managers.time:time("round")
+
+		print("Player joined at round time", player.round_time_joined)
+
+		if self._profiles[profile_id] == nil then
+			self._profiles[profile_id] = {
+				player = player,
+				entities = {},
+				attributes = {},
+				stats = {}
+			}
+		end
+
+		Managers.backend:get_profile_attributes(profile_id, callback(self, "cb_profile_attributes_received", profile_id, player))
+	else
+		print("Ignoring player", player:name(), "with backend profile id", profile_id)
+		CommandWindow.print("[Backend]", "Ignoring player", player:name(), "with backend profile id", profile_id)
+	end
+end
+
+function PersistenceManagerServer:cb_profile_attributes_received(profile_id, player, response)
+	if response.error == nil then
+		printf("Profile attributes received for player %q (%d)", player:name(), player.backend_profile_id)
+
+		local network_id = player:network_id()
+		local profile = self._profiles[profile_id]
+
+		profile.attributes = self:_parse_profile_attributes(response.attributes or {})
+
+		for name, value in pairs(profile.attributes) do
+			local stat = StatsContexts.player[name]
+
+			if stat then
+				if stat.backend.load then
+					self._stats:set(network_id, name, value)
+				end
+			else
+				printf("Stat %q exists in backend but not in StatsContexts.player", name)
+			end
+		end
+
+		if GameSettingsDevelopment.network_mode == "steam" then
+			self._achievements:register_player(player)
+			self._server_browser_score:register_player(player)
+		end
+
+		self._prizes:register_player(player)
+		self._medals:register_player(player)
+		self._ranks:register_player(player)
+
+		for name, value in pairs(profile.attributes) do
+			local stat = StatsContexts.player[name]
+
+			if stat and stat.backend.save and not stat.backend.load then
+				self._stats:trigger(network_id, name, value)
+			end
+		end
+	else
+		printf("No profile attributes received for player %q (%d): %s", player:name(), player.backend_profile_id, response.error)
+
+		self._profiles[player] = nil
+	end
+end
+
+function PersistenceManagerServer:cb_profile_entities_received(profile_id, player, response)
+	if response.error == nil then
+		printf("Profile entities received for player %q (%d)", player:name(), player.backend_profile_id)
+
+		self._profiles[player].entities = self:_parse_profile_entities(response.entities or {})
+	else
+		printf("Error receiving profile entities for player %q (%d): %s", player:name(), player.backend_profile_id, response.error)
+	end
+end
+
+function PersistenceManagerServer:save(callback)
+	print("Save progress", self._settings.save_progress)
+
+	if self:_check_requirements() and self._settings.save_progress then
+		self._callback = callback
+
+		self:_save_profiles()
+		self:_save_telemetry()
+		self._telemetry:finalize()
+	else
+		print("[PersistenceManagerServer] No save performed as we are not connected!")
+		callback()
+	end
+end
+
+function PersistenceManagerServer:_check_requirements()
+	if not Managers.backend:connected() then
+		print("Skipping saving stats to backend due to no connection...")
+
+		return false
+	end
+
+	if Managers.admin:is_server_password_protected() then
+		print("Skipping saving stats to backend due to server being password protected...")
+
+		return false
+	end
+
+	return true
+end
+
+function PersistenceManagerServer:_save_profiles()
+	self._profiles_saving = {}
+
+	local profile_stats_to_save = {}
+
+	for profile_id, profile_data in pairs(self._profiles) do
+		local player = profile_data.player
+		local network_id = player:network_id()
+		local profile_attributes = profile_data.attributes
+		local profile_attributes_to_save = {}
+
+		for stat_name, stat_props in pairs(StatsContexts.player) do
+			if stat_props.backend.save then
+				if stat_props.type == "compound" then
+					profile_stats_to_save[stat_name] = profile_stats_to_save[stat_name] or {}
+
+					local compound_stat = self._stats:get(network_id, stat_name)
+
+					profile_stats_to_save[stat_name][profile_id] = compound_stat:get_backend_data(profile_id)
+				else
+					local stat_value = self._stats:get(network_id, stat_name)
+
+					if stat_props.backend.save_mode == "set" and profile_attributes[stat_name] ~= stat_value then
+						profile_attributes_to_save[stat_name] = {
+							[Backend.PROFILE_ATTRIBUTE_SET] = tostring(stat_value)
+						}
+					elseif stat_props.backend.save_mode == "inc" and stat_value > 0 then
+						profile_attributes_to_save[stat_name] = {
+							[Backend.PROFILE_ATTRIBUTE_INC] = stat_value
+						}
+					elseif type(stat_props.backend.save_mode) == "table" then
+						local save_mode, source_stat = next(stat_props.backend.save_mode)
+						local source_value = self._stats:get(network_id, source_stat)
+
+						profile_attributes_to_save[stat_name] = {
+							[Backend.PROFILE_ATTRIBUTE_INC] = source_value
+						}
+					end
+				end
+			end
+		end
+
+		if not table.is_empty(profile_attributes_to_save) then
+			self._profiles_saving[player] = true
+
+			local callback = callback(self, "cb_attributes_saved", player)
+
+			Managers.backend:update_profile_attributes(profile_id, profile_attributes_to_save, callback)
+		end
+	end
+
+	if not table.is_empty(profile_stats_to_save) and self._settings.stats then
+		cprint("[Backend] Saving profile stats")
+
+		local callback = callback(self, "cb_stats_saved")
+
+		Managers.backend:save_stats(profile_stats_to_save, callback)
+	end
+
+	if table.is_empty(self._profiles) then
+		self._callback()
+	end
+end
+
+function PersistenceManagerServer:_save_telemetry()
+	for profile_id, profile_data in pairs(self._profiles) do
+		self._telemetry:save_round(profile_data.player)
+	end
+end
+
+function PersistenceManagerServer:cb_stats_saved(response)
+	if response.error == nil then
+		printf("Stats saved")
+	else
+		printf("Error saving stats: %s", response.error)
+	end
+end
+
+function PersistenceManagerServer:cb_attributes_saved(player, response)
+	if response.error == nil then
+		printf("Profile attributes saved for player %q (%d)", player:name(), player.backend_profile_id)
+	else
+		printf("Error saving profile attributes for player %q (%d): %s", player:name(), player.backend_profile_id, response.error)
+	end
+
+	self._profiles_saving[player] = nil
+
+	print(table.size(self._profiles_saving), " profiles to save")
+
+	if table.is_empty(self._profiles_saving) then
+		self._callback()
+	end
+end
+
+function PersistenceManagerServer:update(t, dt)
+	local reconnect = self._state == "error" or self._state == "connected" and not Managers.backend:connected()
+
+	if reconnect then
+		Managers.admin:lock_server()
+		self:connect()
+		Managers.chat:send_chat_message(1, "Connection to the backend has been lost. Your progress may not be saved.")
+		Managers.chat:send_chat_message(1, "Reconnecting...")
+	end
+end
+
+function PersistenceManagerServer:destroy()
+	Managers.backend:logout()
+	Managers.backend:disconnect()
+end
