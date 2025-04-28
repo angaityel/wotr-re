99449ecca814951f698aaa946f16c96ff6c4adfd
angaityel <161797572+angaityel@users.noreply.github.com>
2025-04-28 05:25:29 +0300

    Unlock dlc

--- lua/scripts/settings/dlc_settings.lua
+++ lua/scripts/settings/dlc_settings.lua
@@ -13,11 +13,25 @@ local function app_id_check(app_id)
 end
 
 DLCSettings = {
-	house_of_lancaster_armor_set = app_id_check(218841),
-	house_of_york_armor_set = app_id_check(218842),
-	deluxe_edition = app_id_check(218843),
-	brian_blessed = app_id_check(218844),
-	full_game = app_id_check(230940),
-	coins_50k = app_id_check(242070),
-	coins_100k = app_id_check(242090)
+	house_of_lancaster_armor_set = function()
+		return true
+	end,
+	house_of_york_armor_set = function()
+		return true
+	end,
+	deluxe_edition = function()
+		return true
+	end,
+	brian_blessed = function()
+		return true
+	end,
+	full_game = function()
+		return true
+	end,
+	coins_50k = function()
+		return true
+	end,
+	coins_100k = function()
+		return true
+	end
 }
